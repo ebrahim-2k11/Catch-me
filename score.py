0845a151125fa10e623400ac0267ab0232adbeea
ebrahim_2k1 <ebrahimyasser2111@gmail.com>
2025-08-31 16:32:53 +0300

    Update score.py

--- score.py
+++ score.py
@@ -3,17 +3,21 @@ from turtle import Turtle
 class Score(Turtle):
     
     def __init__(self):
-        self.score = 0             
+        self.score = 0     
+        # Added highscore system: reads saved highscore from "Highscore.txt"
+        self.highscore = self.get_high_score()      
         super().__init__()
         self.goto(0, 360)          
         self.color("white")        
         self.penup()            
         self.hideturtle()       
     
+    
     def display_score(self):
-        # Clear previous score and rewrite the current score
+        # Show current score and highscore on screen
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Comic Sans", 27, "normal"))
+        self.write(f"Score: {self.score}\nHighscore: {self.highscore} ", 
+                   align="center", font=("Comic Sans", 27, "normal"))
 
     def add_two_points(self):
         """+2 points when catching a square"""
@@ -31,9 +35,25 @@ class Score(Turtle):
         """+5 points when catching a turtle (except white turtle)"""
         self.score += 5
 
+    def get_high_score(self):
+        # Reads the saved highscore from file
+        with open("Highscore.txt","r") as highscore:
+            return int(highscore.read())
+    
+    def save_high_score(self):
+        # Saves the current score as the new highscore into file
+        with open("Highscore.txt","w") as highscore:
+            highscore.write(str(self.score))
+
     def game_over(self):
-        # Display final "Game Over" message in the center of the screen
+        # At game over, update highscore if current score is higher
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.save_high_score()
+
+        # Display final messages in the center
         self.goto(0, 0)
-        self.write("  Game over", align="center", font=("Comic Sans", 27, "normal"))
+        self.write("  Game over\n", align="center", font=("Comic Sans", 27, "normal"))
         self.goto(0, -35)
-        self.write(f"Your final score {self.score}", align="center", font=("Comic Sans", 27, "normal"))
+        self.write(f"   Your final score {self.score}\n   Your high score {self.highscore}", 
+                   align="center", font=("Comic Sans", 27, "normal"))
